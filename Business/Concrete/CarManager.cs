c784236cfe571bb3978857ee42178532c34d4089
KaleHamza <hamza1kale@gmail.com>
2021-02-28 17:41:25 +0300

    new

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{//Business da asla entity veya framework yok!
+    //Business da uygulanabilecek tek şey ICarDall.
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+                //İş kodları.
+                //Yetki kontrolü.
+                //Geçer ise return e ulaşır ve ürünlere de ulaşmış olur.
+            return _carDal.GetAll();
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{       //IEntity iplement eden class bir veritabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
